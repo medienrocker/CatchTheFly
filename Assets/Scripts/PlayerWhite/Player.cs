8c518c16dc202ac84e8a445c1c455de8f379cf78
medienrocker <50263518+medienrocker@users.noreply.github.com>
2021-02-18 23:37:44 +0100

    Added FootParticleEffects

--- Assets/Scripts/PlayerWhite/Player.cs
+++ Assets/Scripts/PlayerWhite/Player.cs
@@ -6,21 +6,32 @@ public class Player : MonoBehaviour {
 	[SerializeField] float runSpeed = 5f;
 	[SerializeField] float jumpForce = 10f;
 	[SerializeField] float climbSpeed = 10f;
+	
 	float gravityScaleAtStart;
+	bool wasOnGround;
+
 	// States
 	//bool isAlive = true;
 
 	// Caches and references
 	Rigidbody2D myRigidbody2D;
 	Animator myAnimator;
-	Collider2D myCollider2D;
+	[SerializeField] BoxCollider2D myBodyCollider;
+	[SerializeField] BoxCollider2D myFeetCollider;
+
+	[SerializeField] ParticleSystem footsteps;
+	ParticleSystem.EmissionModule footstepsEmission;
+
+	[SerializeField] ParticleSystem impactEffect;
 
 	// Messages then Methods
 	void Start() {
 		myRigidbody2D = GetComponent<Rigidbody2D>();
 		myAnimator = GetComponent<Animator>();
-		myCollider2D = GetComponent<Collider2D>();
+		myBodyCollider = GetComponent<BoxCollider2D>();
 		gravityScaleAtStart = myRigidbody2D.gravityScale;
+
+		footstepsEmission = footsteps.emission;
 	}
 
 	void Update() {
@@ -28,17 +39,20 @@ public class Player : MonoBehaviour {
 		Climb();
 		Jump();
 		FlipPlayerSprite();
+		MakeImpactEffect();
 	}
 
 	void Run() {
 		float moveX = Input.GetAxisRaw("Horizontal");
 		myRigidbody2D.velocity = new Vector2(moveX * runSpeed, myRigidbody2D.velocity.y);
-			bool hasHorizontalSpeed = Mathf.Abs(myRigidbody2D.velocity.x) > Mathf.Epsilon; // means > 0
-			myAnimator.SetBool("isRunning", hasHorizontalSpeed);
+		bool hasHorizontalSpeed = Mathf.Abs(myRigidbody2D.velocity.x) > Mathf.Epsilon; // means > 0
+		myAnimator.SetBool("isRunning", hasHorizontalSpeed);
+		MakeFootstepDust(moveX);
 	}
 
+
 	void Climb() {
-		if (!myCollider2D.IsTouchingLayers(LayerMask.GetMask("Climbing"))) {
+		if (!myFeetCollider.IsTouchingLayers(LayerMask.GetMask("Climbing"))) {
 			myAnimator.SetBool("isClimbing", false);
 			myRigidbody2D.gravityScale = gravityScaleAtStart;
 			return; 
@@ -53,7 +67,7 @@ public class Player : MonoBehaviour {
 	}
 
 	void Jump() {
-		if (!myCollider2D.IsTouchingLayers(LayerMask.GetMask("Ground", "Climbing"))) {
+		if (!myFeetCollider.IsTouchingLayers(LayerMask.GetMask("Ground", "Climbing"))) {
 			myAnimator.SetBool("isJumping", true);
 			myAnimator.SetBool("isRunning", false);
 			return; 
@@ -75,5 +89,27 @@ public class Player : MonoBehaviour {
 		if (hasHorizontalSpeed) {
 			transform.localScale = new Vector2(Mathf.Sign(myRigidbody2D.velocity.x), transform.localScale.y);
 		}
+	}
+
+	// Particle effects
+	void MakeFootstepDust(float movement) {
+		if (Mathf.Abs(movement) > Mathf.Epsilon && myFeetCollider.IsTouchingLayers(LayerMask.GetMask("Ground", "Climbing"))) {
+			footstepsEmission.rateOverTime = 50f;
+		}
+		else {
+			footstepsEmission.rateOverTime = 0f;
+		}
+	}
+
+	void MakeImpactEffect() {
+		bool isGrounded = myFeetCollider.IsTouchingLayers(LayerMask.GetMask("Ground", "Climbing"));
+		if (!wasOnGround && isGrounded) {
+			impactEffect.gameObject.SetActive(true);
+			impactEffect.Stop();
+			//impactEffect.transform.position = transform.position;
+			impactEffect.Play();
+		}
+
+		wasOnGround = isGrounded;
 	}
 }
