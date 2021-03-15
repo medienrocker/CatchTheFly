9186d43dff2354804b7d3025bc24b9a23179e4ef
medienrocker <50263518+medienrocker@users.noreply.github.com>
2021-03-15 20:42:46 +0100

    Added Fly and Butterfly Animation

--- Assets/Scripts/PlayerWhite/Food.cs
+++ Assets/Scripts/PlayerWhite/Food.cs
@@ -5,6 +5,7 @@ public class Food : MonoBehaviour {
 	//[SerializeField] Transform startPoint;
 	[SerializeField] GameObject foodExplosion;
 	[SerializeField] GameObject[] bloodSplashes;
+	[SerializeField] float YOffset = 0f;
 
 	RipplePostProcessor camRipple;
 
@@ -27,6 +28,8 @@ public class Food : MonoBehaviour {
 	void ProcessCollision(GameObject collision) {
 		if (collision.gameObject.tag == "Player") {
 			Instantiate(foodExplosion, transform.position, Quaternion.identity);
+			//var splashPosition = new Vector3(transform.position.x, transform.position.y + YOffset, transform.position.z);
+			//Instantiate(bloodSplashes[Random.Range(0, bloodSplashes.Length)], splashPosition, Quaternion.identity);
 			Instantiate(bloodSplashes[Random.Range(0, bloodSplashes.Length)], transform.position, Quaternion.identity);
 			camRipple.RippleEffect();
 			//transform.position = startPoint.transform.position;
