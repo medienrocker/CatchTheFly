b462ce29c4c5dec89684f1d32fee0ebc18959b9e
medienrocker <50263518+medienrocker@users.noreply.github.com>
2021-03-18 10:28:02 +0100

    Added Pulsating effect script

---
+++ Assets/Scripts/PlayerWhite/PulsatingEffect.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class PulsatingEffect : MonoBehaviour {
+
+	[Header("Pulse Variables")]
+	public AnimationCurve expandCurve;
+	public float expandAmount, expandSpeed;
+
+	Vector3 startSize;
+	Vector3 targetSize;
+	float scrollAmount;
+	
+	void Start() {
+		InitPulseEffectVariables();
+	}
+
+	void InitPulseEffectVariables() {
+		startSize = transform.localScale;
+		targetSize = startSize * expandAmount;
+	}
+
+	void Update() {
+		MakeObjectPulse();
+	}
+
+	public void MakeObjectPulse() {
+		scrollAmount += Time.deltaTime * expandSpeed;
+		float _percent = expandCurve.Evaluate(scrollAmount);
+		transform.localScale = Vector2.Lerp(startSize, targetSize, _percent);
+	}
+}
